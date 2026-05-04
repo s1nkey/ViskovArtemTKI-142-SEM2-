4921ee4b1c44c35c50873d7d0f8fca94d5b2da64
Артём Висков ТКИ-142 <98798531+s1nkey@users.noreply.github.com>
2026-05-05 00:49:42 +0300

    Update Vector.cpp

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,18 +1,18 @@
 #include "../header/Vector.h"
 #include <math.h>
 
-Vector::Vector(const Point A, const Point B)
+Vector::Vector(const Point& A, const Point& B);
 {
 	this->A = A;
 	this->B = B;
 }
 
-double Vector::getLen()
+double Vector::getLen() const;
 {
 	return sqrt(pow(B.getX() - A.getX(), 2) + pow(B.getY() - A.getY(), 2));
 }
 
-double Vector::getAngle(Vector& other)
+double Vector::getAngle(const Vector& other) const;
 {
     double x1 = B.getX() - A.getX();
     double y1 = B.getY() - A.getY();
@@ -24,7 +24,7 @@ double Vector::getAngle(Vector& other)
     double len1 = sqrt(x1 * x1 + y1 * y1);
     double len2 = sqrt(x2 * x2 + y2 * y2);
 
-    if (len1 == 0 || len2 == 0)
+    if (len1 <= std::numeric_limits<double>::epsilon() || len2 <= std::numeric_limits<double>::epsilon())
     {
         throw std::runtime_error("Невозможно вычислить угол: нулевой вектор");
     }
