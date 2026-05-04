e4c60f560dc974cc41b8f848c8ab3a07b7e17e36
Артём Висков ТКИ-142 <98798531+s1nkey@users.noreply.github.com>
2026-05-05 01:40:39 +0300

    Update Vector.h

--- header/Vector.h
+++ header/Vector.h
@@ -22,12 +22,12 @@ public:
 	 * @brief Вычисляет длину вектора
 	 * @return Длина вектора
 	 */
-	double getLen();
+	double getLen() const;
 
 	/**
 	 * @brief Вычисляет угол между текущим вектором и другим вектором
 	 * @param other - Другой вектор, с которым вычисляется угол
 	 * @return Угол между векторами
 	 */
-	double getAngle(Vector& other);
+	double getAngle(const Vector& other) const;
 };
